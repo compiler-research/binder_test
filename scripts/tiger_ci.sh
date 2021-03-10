867a96c203790d670b963bb9cc227905cbbc9e91
David Lange <David.Lange@cern.ch>
2021-03-10 11:34:13 +0100

    Update tiger_ci.sh

--- scripts/tiger_ci.sh
+++ scripts/tiger_ci.sh
@@ -2,9 +2,11 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=28
-#SBATCH --exclusive
 #SBATCH --gres=gpu:1
 #SBATCH -t 01:00:00
+#SBATCH --nodes=1
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --mail-user=dlange@princeton.edu
 
 module load cudatoolkit/10.0
 module load rh/devtoolset/8
